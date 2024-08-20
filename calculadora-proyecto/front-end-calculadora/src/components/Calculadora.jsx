13efb85ca047811ca7b2e3fd036f0d80824f1cfe
edwinbravo34 <edwinbravo2121@gmail.com>
2024-08-20 14:59:12 -0500

    agregar boton

--- calculadora-proyecto/front-end-calculadora/src/components/Calculadora.jsx
+++ calculadora-proyecto/front-end-calculadora/src/components/Calculadora.jsx
@@ -2,20 +2,20 @@ import { useState } from "react";
 import '../styles/Calculadora.css'
 import Resultado from "./Resultado";
 
-function Calculadora(){
+function Calculadora() {
     const [number1, setNumber1] = useState('');
     const [number2, setNumber2] = useState('');
     const [resultado, setResultado] = useState('');
 
-    function handleSubmit(e){
+    function handleSubmit(e) {
         e.preventDefault();
         const operacion = e.target.value;
         fetch(`http://localhost:3500/v1/calculadora/${operacion}`, {
             method: 'POST',
-            headers: {"Content-Type": "application/json"},
-            body: JSON.stringify({number1, number2})
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ number1, number2 })
         })
-            .then(res =>res.json())
+            .then(res => res.json())
             .then(responseData => {
                 setResultado(responseData.resultado)
                 // setResultado(responseData)
@@ -27,13 +27,14 @@ function Calculadora(){
         <div className="container">
             <h1 id="txtCalculadora">CALCULADORA</h1>
             <form>
-                <input type="text" className="number" onChange={(e)=>{setNumber1(e.target.value)}}/><br />
-                <input type="text" className="number" onChange={(e)=>{setNumber2(e.target.value)}}/><br />
-                <input type="submit" className="btnEnviar" value="sumar" onClick={handleSubmit}/>
-                <input type="submit" className="btnEnviar" value="restar" onClick={handleSubmit}/>
-                <input type="submit" className="btnEnviar" value="multiplicar" onClick={handleSubmit}/>
+                <input type="text" className="number" onChange={(e) => { setNumber1(e.target.value) }} /><br />
+                <input type="text" className="number" onChange={(e) => { setNumber2(e.target.value) }} /><br />
+                <input type="submit" className="btnEnviar" value="sumar" onClick={handleSubmit} />
+                <input type="submit" className="btnEnviar" value="restar" onClick={handleSubmit} />
+                <input type="submit" className="btnEnviar" value="multiplicar" onClick={handleSubmit} />
+                <input type="submit" className="btnEnviat" value="dividir" onclick={handleSubmit} />
             </form>
-            <Resultado resultado={"El resultado es "+ resultado}/>
+            <Resultado resultado={"El resultado es " + resultado} />
         </div>
     )
 }
