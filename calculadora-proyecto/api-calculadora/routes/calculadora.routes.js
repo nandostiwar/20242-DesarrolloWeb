2b4afb9e90479b384753407a34b945f8a46c4827
Miguel <miguel.arango02>
2024-08-27 15:27:07 -0500

    Error pequeño

--- calculadora-proyecto/api-calculadora/routes/calculadora.routes.js
+++ calculadora-proyecto/api-calculadora/routes/calculadora.routes.js
@@ -8,6 +8,6 @@ router
     .post('/multiplicar', calculadoraControllers.multiplicar)
     .post('/mayor', calculadoraControllers.mayor)
     .post('/menor', calculadoraControllers.menor)
-    .post('/promedi', calculadoraControllers.promedio)
+    .post('/promedio', calculadoraControllers.promedio)
 
 module.exports = router;
