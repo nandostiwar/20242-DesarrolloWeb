6eb00ae7a8da4a2c5123da2442b2023a76c976aa
Miguel <miguel.arango02>
2024-08-27 15:22:34 -0500

    Actulizacion de las funciones de la calculadora

--- calculadora-proyecto/api-calculadora/operaciones/operaciones.js
+++ calculadora-proyecto/api-calculadora/operaciones/operaciones.js
@@ -23,9 +23,38 @@ function multiply(a, b){
     return number1 * number2;
 }
 
+function greatherthan(a, b) {
+    let number1 = parseInt(a);
+    let number2 = parseInt(b);
+    if (number1 > number2) {
+        return number1;
+    } else {
+        return number2;
+    }
+}
+
+
+function lessthan(a, b){
+    let number1 = parseInt(a);
+    let number2 = parseInt(b);
+    if (number1 < number2) {
+        return number1;
+    } else {
+        return number2;
+    }
+}
+
+function average(a, b) {
+    let number1 = parseInt(a);
+    let number2 = parseInt(b);
+    return (number1 + number2) / 2;
+}
 
 module.exports = {
     add,
     subtract,
-    multiply
+    multiply,
+    greatherthan,
+    lessthan,
+    average
 }
