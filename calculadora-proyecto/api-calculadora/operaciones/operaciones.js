5ce7cac351dd8d4a5804f63bfc8187bec87b60d6
edwinbravo34 <edwinbravo2121@gmail.com>
2024-08-27 16:22:04 -0500

    agregar funciones

--- calculadora-proyecto/api-calculadora/operaciones/operaciones.js
+++ calculadora-proyecto/api-calculadora/operaciones/operaciones.js
@@ -1,30 +1,68 @@
-
 /**
  * Sumar dos cantidades numéricas
  * @param {Number} a 
  * @param {Number} b 
  * @returns Number
  */
-function add(a, b){
+function add(a, b) {
     let number1 = parseInt(a);
     let number2 = parseInt(b);
     return number1 + number2;
 }
 
-function subtract(a, b){
+function subtract(a, b) {
     let number1 = parseInt(a);
     let number2 = parseInt(b);
     return number1 - number2;
 }
 
-function multiply(a, b){
+function multiply(a, b) {
     let number1 = parseInt(a);
     let number2 = parseInt(b);
     return number1 * number2;
 }
 
+/**
+ * Obtener el número mayor de dos cantidades numéricas
+ * @param {Number} a 
+ * @param {Number} b 
+ * @returns Number
+ */
+function max(a, b) {
+    let number1 = parseInt(a);
+    let number2 = parseInt(b);
+    return Math.max(number1, number2);
+}
+
+/**
+ * Obtener el número menor de dos cantidades numéricas
+ * @param {Number} a 
+ * @param {Number} b 
+ * @returns Number
+ */
+function min(a, b) {
+    let number1 = parseInt(a);
+    let number2 = parseInt(b);
+    return Math.min(number1, number2);
+}
+
+/**
+ * Obtener el promedio de dos cantidades numéricas
+ * @param {Number} a 
+ * @param {Number} b 
+ * @returns Number
+ */
+function average(a, b) {
+    let number1 = parseInt(a);
+    let number2 = parseInt(b);
+    return (number1 + number2) / 2;
+}
+
 module.exports = {
     add,
     subtract,
-    multiply
+    multiply,
+    max,
+    min,
+    average
 }
