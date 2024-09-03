e5ece4051951098f70579bf773bf42ebc3116b99
jairomoran01 <jairo.moran01@unicatolica.edu.co>
2024-09-03 14:09:55 -0500

    Seleccionable y editable por perfil el horoscopo

--- proyecto-general-horoscopo/api-json/routes/controllers/signoController.js
+++ proyecto-general-horoscopo/api-json/routes/controllers/signoController.js
@@ -1,34 +1,42 @@
 const fs = require('fs/promises');
 const path = require('path');
 
-const getAllSignos = async (req, res)=>{
-    const signo = await fs.readFile(path.join(__dirname,'../../db/signos.json'));
+const getAllSignos = async (req, res) => {
+    const signo = await fs.readFile(path.join(__dirname, '../../db/signos.json'));
     const signosJson = JSON.parse(signo)
     res.json(signosJson);
 }
 
-const getOneSigno = async (req, res)=>{
+const getOneSigno = async (req, res) => {
     const oneSigno = req.params.signo;
-    const allSignos = await fs.readFile(path.join(__dirname,'../../db/signos.json'));
+    const perfil = req.query.perfil || 'general'; // Nuevo parámetro de consulta para el perfil
+    const allSignos = await fs.readFile(path.join(__dirname, '../../db/signos.json'));
     const objSignos = JSON.parse(allSignos);
-    const result = objSignos[oneSigno];
+    const result = objSignos[oneSigno] && objSignos[oneSigno][perfil] 
+        ? objSignos[oneSigno][perfil] 
+        : "Información no disponible para este perfil";
     res.json(result)
 }
 
-const updateSigno = async (req, res)=>{
+const updateSigno = async (req, res) => {
     const signoEditar = req.params.signoEditar;
-    const {textoEditar} = req.body;
-    const allSignos = await fs.readFile(path.join(__dirname,'../../db/signos.json'));
+    const { textoEditar, perfil } = req.body;
+    const allSignos = await fs.readFile(path.join(__dirname, '../../db/signos.json'));
     const objSignos = JSON.parse(allSignos);
-
+    
+    if (!objSignos[signoEditar]) {
+        objSignos[signoEditar] = {};
+    }
+    
     const objUpdate = {
         ...objSignos,
-        [signoEditar]: textoEditar
+        [signoEditar]: {
+            ...objSignos[signoEditar],
+            [perfil]: textoEditar
+        }
     }
-
-    // console.log(objUpdate);
-    await fs.writeFile(path.join(__dirname,'../../db/signos.json'), JSON.stringify(objUpdate, null, 2), {encoding: 'utf-8'})
-
+    
+    await fs.writeFile(path.join(__dirname, '../../db/signos.json'), JSON.stringify(objUpdate, null, 2), {encoding: 'utf-8'})
     res.json({
         message: "Updated"
     })
