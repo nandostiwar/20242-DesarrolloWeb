d8e164e61af6c3630b7025c264d46a1ed4ad4639
jairomoran01 <jairo.moran01@unicatolica.edu.co>
2024-09-17 16:28:01 -0500

    Parcial 1 Listo

--- calculadora-proyecto/api-calculadora/routes/calculadora.routes.js
+++ calculadora-proyecto/api-calculadora/routes/calculadora.routes.js
@@ -3,11 +3,8 @@ const router = express.Router();
 const calculadoraControllers = require('../controllers/calculadoraControllers.js');
 
 router
-    .post('/sumar', calculadoraControllers.sumar)
-    .post('/restar', calculadoraControllers.restar)
-    .post('/multiplicar', calculadoraControllers.multiplicar)
-    .post('/mayor', calculadoraControllers.mayor)
-    .post('/menor', calculadoraControllers.menor)
-    .post('/promedio', calculadoraControllers.promedio)
+    .post('/sortAsc', calculadoraControllers.ordenarAscendente)    // Ordenar de forma ascendente
+    .post('/sortDesc', calculadoraControllers.ordenarDescendente)  // Ordenar de forma descendente
+    .post('/equation', calculadoraControllers.resolverEcuacion)    // Resolver ecuaciones
 
 module.exports = router;
